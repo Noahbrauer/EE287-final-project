1757bb4fdacc320fdc840092419b7306220b04ab
gwba224 <38711687+gwba224@users.noreply.github.com>
2018-04-25 16:10:06 -0400

    1.2

--- PortE.c
+++ PortE.c
@@ -20,3 +20,16 @@ void PortE_Init(void){
 	ADC0_SSCTL3_R = 0x0006;
 	ADC0_ACTSS_R |= 0x0008;
 }
+void PortF_Init(void){
+	volatile unsigned long delay;
+	SYSCTL_RCGC2_R |= 0x00000020;
+	delay = SYSCTL_RCGC2_R;
+	GPIO_PORTF_LOCK_R = 0x4C4F434B;
+	GPIO_PORTF_CR_R = 0x1F;
+	GPIO_PORTF_AFSEL_R &= ~0x10;
+	GPIO_PORTF_PCTL_R = 0x00000000;
+	GPIO_PORTF_DIR_R &= ~0x10;
+	GPIO_PORTF_AMSEL_R = 0x00;
+	GPIO_PORTF_PUR_R = 0x10;
+	GPIO_PORTF_DEN_R |= 0x10;
+}
